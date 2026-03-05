c00c53828a62525d4be9be72c9b81c1fdd984d1d
unknown <Josiah Calderon>
2026-03-05 23:53:14 +0800

    Added new options - lucky pick and void. Turns menu into module

--- lottery_draw_machine.py
+++ lottery_draw_machine.py
@@ -1,78 +1,8 @@
 import random
+import lottery_login
+import lottery_menu
 
-def menu():
-    menu = ["P", "I"]
-    while True:
-        print("""--------------------
-        MENU
-
-    [P] Play
-    [I] Instruction
-        """)
-        menu_select = input("""--------------------
-Select menu: """).upper()
-        print("--------------------")
-        if menu_select in menu:
-            if menu_select == "P":
-                break
-            else:
-                instruction()
-        else:
-            print("Invalid menu option.")
-
-# Gamemode selector
-def gamemode():
-    while True:
-        try:
-            print("""Gamemodes:
-[1] 6/42 -  Players draw six numbers from a pool of 42. 
-            The jackpot is the largest among these games, 
-            with a minimum guaranteed amount of ₱10,000.00 for the 1st Prize.
-            The game is considered easier to win compared to others with larger pools.
-[2] 6/45 -  Players draw six numbers from a pool of 45. 
-            The jackpot amount varies based on ticket sales, 
-            making it more challenging to win compared to the 6/42 game. 
-[3] 6/49 -  Players draw six numbers from a pool of 49.
-            The jackpot amount also varies based on ticket sales, similar to the 6/45 game.
-[4] 6/55 -  Players draw six numbers from a pool of 55.
-            The jackpot amount varies based on ticket sales,
-            making it more challenging to win compared to the previous games. 
-[5] 6/58 -  Players draw six numbers from a pool of 58.
-            The jackpot amount varies based on ticket sales, making it the most challenging game to win. 
-
-[0] Back
-            """)
-            gamemode = int(input("Please select the number of gamemode (1-5): "))
-            print("--------------------")
-            if gamemode == 0:
-                main()
-            elif gamemode == 1:
-                return 42
-            elif gamemode == 2:
-                return 45
-            elif gamemode == 3:
-                return 49
-            elif gamemode == 4:
-                return 55
-            elif gamemode == 5:
-                return 58
-            else:
-                print("Choose from 1 to 5 only.")
-        except ValueError:
-            print("Input a valid number.")
-
-def instruction():
-    print("""
-Instructions:
-1. Choose a gamemode.
-2. Enter 6 different numbers.
-3. The system will generate winning numbers.
-4. If your numbers match, you win!
-
-(Winner prizes vary depending on gamemodes.)
-""")
-    return
-
+#Lottery drawing
 class lottery_drawing:
     @staticmethod
     def winner_ticket(total_draws, max_ball_number):
@@ -80,12 +10,22 @@ class lottery_drawing:
         return sorted(lucky_ticket)
 
     @staticmethod
-    def user_ticket(total_draws, max_ball_number):
+    def add_ticket(total_draws, max_ball_number):
         user_bets = []
+        print("""
+[L] Lucky Pick
+[V] Void
+        """)
         while len(user_bets) < total_draws:
             try:
-                bet = int(input(f"Bet Number {len(user_bets) + 1}: "))
-                if bet > max_ball_number or bet < 1:
+                bet = input(f"Bet Number {len(user_bets) + 1}: ")
+                if bet.upper() == "L":
+                    user_bets.append(random.randrange(1, max_ball_number + 1))
+                elif bet.upper() == "V":
+                    print("Your draw has been voided.")
+                    break
+                
+                elif int(bet) > max_ball_number or int(bet) < 1:
                     print(f"Your number must be from 1 to {max_ball_number}")
                 elif bet in user_bets:
                     print("You've already bet that number.")
@@ -97,14 +37,20 @@ class lottery_drawing:
 
 # -----MAIN PROGRAM-----
 def main():
-    menu()
-    total_draws = 6
-    max_ball_number = gamemode()
-    ticket = lottery_drawing.user_ticket(total_draws, max_ball_number)
-    drawn_ticket = lottery_drawing.winner_ticket(total_draws, max_ball_number)
-    print("\nYour Ticket:", ticket)
-    print("Winning Ticket:", drawn_ticket)
+    lottery_login.front_panel()
+    while True:    
+        lottery_menu.menu()
+        total_draws = 6
+        max_ball_number = lottery_menu.gamemode()
+        ticket = lottery_drawing.add_ticket(total_draws, max_ball_number)
+        drawn_ticket = lottery_drawing.winner_ticket(total_draws, max_ball_number)
+        print(f"\n{ticket}\n")
+        print(f"{drawn_ticket}")
+        continue
+
 
 if __name__ == "__main__":
     print("WELCOME TO LOTTERY DRAW MACHINE")
     main()
+
+
